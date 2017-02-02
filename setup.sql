ce6c4b5dce614674dbcfe2600d02e4f95c658604
Pierre <pierre.rochard@axial.net>
2017-02-02 14:40:10 -0500

    adding SQL setup script

--- setup.sql
+++ setup.sql
@@ -1,106 +1,133 @@
-ALTER DATABASE rest SET "app.jwt_secret" TO 'SECRET';
+ALTER DATABASE rest SET "app.jwt_secret" TO '4S7lR9SnY8g3';
 ALTER DATABASE rest SET "app.jwt_hours" TO 24;
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE EXTENSION IF NOT EXISTS pgjwt;
 
-create schema if not exists auth;
-create schema if not exists api;
+CREATE SCHEMA IF NOT EXISTS auth;
+CREATE SCHEMA IF NOT EXISTS api;
 
-create table
-auth.users (
-  email    text primary key check ( email ~* '^.+@.+\..+$' ),
-  password text not null check (length(password) < 512),
-  role     name not null check (length(role) < 512)
+CREATE ROLE anon;
+CREATE ROLE authenticator NOINHERIT;
+GRANT anon TO authenticator;
+
+GRANT USAGE ON SCHEMA public, auth TO anon;
+GRANT SELECT ON TABLE pg_authid, auth.users TO anon;
+GRANT EXECUTE ON FUNCTION api.login(TEXT, TEXT) TO anon;
+
+
+CREATE TABLE
+  auth.users (
+  email    TEXT PRIMARY KEY CHECK ( email ~* '^.+@.+\..+$' ),
+  password TEXT NOT NULL CHECK (length(password) < 512),
+  role     NAME NOT NULL CHECK (length(role) < 512)
 );
 
-create or replace function
-auth.check_if_role_exists() returns trigger
-  language plpgsql
-  as $$
-begin
-  if not exists (select 1 from pg_roles where pg_roles.rolname = NEW.role) then
-    raise foreign_key_violation using message = 'Unknown database role: ' || NEW.role;
-    return null;
-  end if;
-  return NEW;
-end
+CREATE OR REPLACE FUNCTION
+  auth.check_if_role_exists()
+  RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  IF NOT exists(SELECT 1
+                FROM pg_roles
+                WHERE pg_roles.rolname = NEW.role)
+  THEN
+    RAISE foreign_key_violation
+    USING MESSAGE = 'Unknown database role: ' || NEW.role;
+    RETURN NULL;
+  END IF;
+  RETURN NEW;
+END
 $$;
 
-drop trigger if exists ensure_user_role_exists on auth.users;
-create constraint trigger ensure_user_role_exists
-  after insert or update on auth.users
-  for each row
-  execute procedure auth.check_if_role_exists();
+DROP TRIGGER IF EXISTS ensure_user_role_exists
+ON auth.users;
+CREATE CONSTRAINT TRIGGER ensure_user_role_exists
+AFTER INSERT OR UPDATE ON auth.users
+FOR EACH ROW
+EXECUTE PROCEDURE auth.check_if_role_exists();
 
 
-create or replace function
-auth.encrypt_password() returns trigger
-  language plpgsql
-  as $$
-begin
-  if tg_op = 'INSERT' or new.password <> old.password then
+CREATE OR REPLACE FUNCTION
+  auth.encrypt_password()
+  RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  IF tg_op = 'INSERT' OR new.password <> old.password
+  THEN
     new.password = crypt(new.password, gen_salt('bf', 8));
-  end if;
-  return new;
-end
+  END IF;
+  RETURN new;
+END
 $$;
 
-drop trigger if exists encrypt_password on auth.users;
-create trigger encrypt_password
-  before insert or update on auth.users
-  for each row
-  execute procedure auth.encrypt_password();
+DROP TRIGGER IF EXISTS encrypt_password
+ON auth.users;
+CREATE TRIGGER encrypt_password
+BEFORE INSERT OR UPDATE ON auth.users
+FOR EACH ROW
+EXECUTE PROCEDURE auth.encrypt_password();
 
-create or replace function
-auth.user_role(email text, password text) returns name
-language plpgsql
-as $$
-  begin
-    return (
-      select role from auth.users
-       where users.email = email
-         and users.password = crypt(password, users.password)
-    );
-  end;
+CREATE OR REPLACE FUNCTION
+  auth.user_role(email TEXT, password TEXT)
+  RETURNS NAME
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN (
+    SELECT role
+    FROM auth.users
+    WHERE users.email = email
+          AND users.password = crypt(password, users.password)
+  );
+END;
 $$;
 
 CREATE TYPE auth.jwt_token AS (
-  token text
+  token TEXT
 );
 
-create or replace function
-api.login(email text, pass text) returns auth.jwt_token
-  language plpgsql
-  as $$
-declare
-  _role name;
+CREATE OR REPLACE FUNCTION
+  api.login(email TEXT, pass TEXT)
+  RETURNS auth.jwt_token
+LANGUAGE plpgsql
+AS $$
+DECLARE
+  _role  NAME;
   result auth.jwt_token;
-begin
-  select auth.user_role(email, pass) into _role;
-  if _role is null then
-    raise invalid_password using message = 'Invalid email or password';
-  end if;
+BEGIN
+  SELECT auth.user_role(email, pass)
+  INTO _role;
+  IF _role IS NULL
+  THEN
+    RAISE invalid_password
+    USING MESSAGE = 'Invalid email or password';
+  END IF;
 
-  select sign(row_to_json(r), current_setting('app.jwt_secret')) as token
-    from (
-      select _role as role, email as email,
-         extract(epoch from now())::integer + current_setting('app.jwt_hours')*60*60 as exp
-    ) r
-    into result;
-  return result;
-end;
+  SELECT sign(row_to_json(r), current_setting('app.jwt_secret')) AS token
+  FROM (
+         SELECT
+           _role                                                                             AS role,
+           email                                                                             AS email,
+           extract(EPOCH FROM now()) :: INTEGER + current_setting('app.jwt_hours') * 60 * 60 AS exp
+       ) r
+  INTO result;
+  RETURN result;
+END;
 $$;
 
 CREATE TABLE api.messages (
-  id           UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  time         TIMESTAMP NOT NULL DEFAULT now(),
-  from_user    NAME      NOT NULL DEFAULT current_user,
-  to_user      NAME      NOT NULL,
-  subject      VARCHAR(64) NOT NULL,
-  body         TEXT
+  id        UUID PRIMARY KEY                                                                                                                                                                                                                                                                                     DEFAULT gen_random_uuid(),
+  time      TIMESTAMP   NOT NULL                                                                                                                                                                                                                                                                                   DEFAULT now(),
+  from_user NAME      NOT NULL                                                                                                                                                                                                                                                                                   DEFAULT current_user,
+  to_user   NAME      NOT NULL,
+  subject   VARCHAR(64) NOT NULL,
+  body      TEXT
 );
 
-CREATE POLICY chat_policy ON api.messages
-  USING ((to_user = current_user) OR (from_user = current_user))
-  WITH CHECK (from_user = current_user)
+CREATE POLICY message_policy
+  ON api.messages
+USING ((to_user = current_user) OR (from_user = current_user))
+WITH CHECK (from_user = current_user)
