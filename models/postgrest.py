073a0ca0565af7b23622f03c3c59f47e948b873f
Pierre Rochard <pierre.rochard@axial.net>
2017-03-07 08:36:36 -0500

    creating a settings table

---
+++ models/postgrest.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, String, text, UniqueConstraint
+from sqlalchemy.dialects.postgresql import UUID
+
+from models import Base
+
+
+class Settings(Base):
+    __tablename__ = 'users'
+    __table_args__ = ({'schema': 'admin'},
+                      UniqueConstraint('user',
+                                       'table',
+                                       'column',
+                                       'key',
+                                       name='setting_unique_constraint'))
+
+    id = Column(UUID(as_uuid=True),
+                server_default=text('gen_random_uuid()'),
+                primary_key=True)
+    user = Column(String)
+    table = Column(String)
+    column = Column(String)
+    key = Column(String)
+    value = Column(String)
