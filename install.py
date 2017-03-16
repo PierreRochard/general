d67d2a358ddde92f05354f3fdc95af2dc42e7d79
Pierre Rochard <pierre.rochard@axial.net>
2017-03-15 19:10:56 -0500

    debugging install

--- install.py
+++ install.py
@@ -204,6 +204,75 @@ def setup_database(database, user):
     GRANT EXECUTE ON FUNCTION api.login(TEXT, TEXT) TO anon;
     ''')
 
+    session.execute('''
+    CREATE OR REPLACE FUNCTION table_notify() RETURNS trigger AS $$
+    DECLARE
+      id UUID;
+      payload text;
+      json_record JSON;
+      payload_size INT;
+    BEGIN
+      IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
+        id = NEW.id;
+        json_record = row_to_json(NEW);
+
+    --   Creates a DIFF from the OLD row to NEW row on updates, and create a change feed
+    --   ELSEIF  TG_OP = 'UPDATE' THEN
+    --     id = NEW.id;
+    --     json_record = jsonb_diff_val(row_to_json(NEW)::JSONB, row_to_json(OLD)::JSONB);
+
+      ELSE
+        id = OLD.id;
+        json_record = row_to_json(OLD);
+      END IF;
+      payload = json_build_object('table_name', TG_TABLE_SCHEMA || '.' || TG_TABLE_NAME, 'id', id, 'type', TG_OP, 'row', json_record)::text;
+      payload_size = octet_length(payload);
+      IF payload_size >= 8000 THEN
+        payload = json_build_object('table_name', TG_TABLE_SCHEMA || '.' || TG_TABLE_NAME, 'id', id, 'type', TG_OP)::text;
+      END IF;
+      PERFORM pg_notify('messages_table_update', payload);
+      RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+
+
+    CREATE OR REPLACE FUNCTION jsonb_diff_val(val1 JSONB,val2 JSONB)
+    RETURNS JSONB AS $$
+    DECLARE
+      result JSONB;
+      v RECORD;
+    BEGIN
+       result = val1;
+       FOR v IN SELECT * FROM jsonb_each(val2) LOOP
+         IF result @> jsonb_build_object(v.key,v.value)
+            THEN result = result - v.key;
+         ELSIF result ? v.key THEN CONTINUE;
+         ELSE
+            result = result || jsonb_build_object(v.key,'null');
+         END IF;
+       END LOOP;
+       RETURN result;
+    END;
+    $$ LANGUAGE plpgsql;
+    ''')
+
+    session.execute('''
+    DROP TRIGGER IF EXISTS messages_notify_update ON api.messages;
+    CREATE TRIGGER messages_notify_update
+    AFTER UPDATE ON api.messages
+    FOR EACH ROW EXECUTE PROCEDURE table_notify();
+
+    DROP TRIGGER IF EXISTS messages_notify_insert ON api.messages;
+    CREATE TRIGGER messages_notify_insert
+    AFTER INSERT ON api.messages
+    FOR EACH ROW EXECUTE PROCEDURE table_notify();
+
+    DROP TRIGGER IF EXISTS messages_notify_delete ON api.messages;
+    CREATE TRIGGER messages_notify_delete
+    AFTER DELETE ON api.messages
+    FOR EACH ROW EXECUTE PROCEDURE table_notify();
+    ''')
+
 if __name__ == '__main__':
     argument_parser = argparse.ArgumentParser(description='Install PostgREST Auth')
     argument_parser.add_argument('-d',  help='Database name', dest='database')
--- models/admin.py
+++ models/admin.py
@@ -31,7 +31,8 @@ class Messages(Base):
                 server_default=text('gen_random_uuid()'),
                 primary_key=True)
     time = Column(DateTime, nullable=False, server_default=text('now()'))
-    from_user = Column(String, nullable=False, server_default=text('current_user'))
+    from_user = Column(String, nullable=False,
+                       server_default=text('current_user'))
     to_user = Column(String, nullable=False)
     subject = Column(String, nullable=False)
     body = Column(String)
