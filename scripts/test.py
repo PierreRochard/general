87b3aba94a0eee04de0986688c8a5b7dab343ede
Pierre Rochard <pierre.rochard@axial.net>
2017-03-18 09:33:38 -0500

    reorganizing

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,21 @@
+import argparse
+
 from models import get_session
 from models.admin import Messages
 
-session = get_session()
 
-for i in range(0, 50):
-    new_message = Messages(to_user='you', subject='test')
-    session.add(new_message)
-    session.commit()
+def insert_records(database, user):
+    session = get_session(database, user)
+    for i in range(0, 50):
+        new_message = Messages(to_user='you', subject='test')
+        session.add(new_message)
+        session.commit()
+
+
+if __name__ == '__main__':
+    argument_parser = argparse.ArgumentParser(
+        description='Create test records')
+    argument_parser.add_argument('-d', help='Database name', dest='database')
+    argument_parser.add_argument('-u', help='Database user', dest='user')
+    args = argument_parser.parse_args()
+    insert_records(args.database, args.user)
