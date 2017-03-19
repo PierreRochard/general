53e3d1705a8ea988ebd91014d333b1cec765fecb
Pierre Rochard <pierre.rochard@axial.net>
2017-03-19 15:30:11 -0500

    refactoring

--- models/admin.py
+++ models/admin.py
@@ -1,38 +1,24 @@
-from sqlalchemy import Boolean, Column, DateTime, Integer, String, text
+from sqlalchemy import Column, String, text, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
 
 from models import Base
 
 
-class Users(Base):
+class Settings(Base):
     __tablename__ = 'users'
-    __table_args__ = {'schema': 'auth'}
+    __table_args__ = (UniqueConstraint('user',
+                                       'path',
+                                       'property',
+                                       'key',
+                                       name='setting_unique_constraint'),
+                      {'schema': 'admin'},
+                      )
 
     id = Column(UUID(as_uuid=True),
                 server_default=text('gen_random_uuid()'),
                 primary_key=True)
-    email = Column(String, unique=True)
-    password = Column(String)
-    role = Column(String)
-    active = Column(Boolean)
-    confirmed_at = Column(DateTime(timezone=True))
-    last_login_at = Column(DateTime(timezone=True))
-    current_login_at = Column(DateTime(timezone=True))
-    last_login_ip = Column(String)
-    current_login_ip = Column(String)
-    login_count = Column(Integer)
-
-
-class Messages(Base):
-    __tablename__ = 'messages'
-    __table_args__ = {'schema': 'api'}
-
-    id = Column(UUID(as_uuid=True),
-                server_default=text('gen_random_uuid()'),
-                primary_key=True)
-    time = Column(DateTime, nullable=False, server_default=text('now()'))
-    from_user = Column(String, nullable=False,
-                       server_default=text('current_user'))
-    to_user = Column(String, nullable=False)
-    subject = Column(String, nullable=False)
-    body = Column(String)
+    user = Column(String)
+    path = Column(String)
+    property = Column(String)
+    key = Column(String)
+    value = Column(String)
---
+++ models/auth.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, text
+from sqlalchemy.dialects.postgresql import UUID
+
+from models import Base
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    __table_args__ = {'schema': 'auth'}
+
+    id = Column(UUID(as_uuid=True),
+                server_default=text('gen_random_uuid()'),
+                primary_key=True)
+    email = Column(String, unique=True)
+    password = Column(String)
+    role = Column(String)
+    active = Column(Boolean)
+    confirmed_at = Column(DateTime(timezone=True))
+    last_login_at = Column(DateTime(timezone=True))
+    current_login_at = Column(DateTime(timezone=True))
+    last_login_ip = Column(String)
+    current_login_ip = Column(String)
+    login_count = Column(Integer)
