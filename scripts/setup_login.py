52130835f34d50945c9ee9c3c53dd8c0130db131
Pierre Rochard <pierre.rochard@axial.net>
2017-03-19 15:07:42 -0500

    refactoring

---
+++ scripts/setup_login.py
@@ -0,0 +1,35 @@
+def install_login_function(session):
+    session.execute(
+        """
+    CREATE OR REPLACE FUNCTION
+      api.login(email TEXT, password TEXT)
+      RETURNS auth.jwt_token
+    LANGUAGE plpgsql
+    AS $$
+    DECLARE
+      _role  NAME;
+      result auth.jwt_token;
+    BEGIN
+      SELECT auth.user_role(email, password)
+      INTO _role;
+      IF _role IS NULL
+      THEN
+        RAISE invalid_password
+        USING MESSAGE = 'Invalid email or password';
+      END IF;
+
+      SELECT sign(row_to_json(r), current_setting('app.jwt_secret')) AS token
+      FROM (
+             SELECT
+               _role            AS role,
+               email            AS email,
+               extract(EPOCH FROM now()) :: INTEGER + current_setting('app.jwt_hours')::INTEGER * 60 * 60 AS exp
+           ) r
+      INTO result;
+      RETURN result;
+    END;
+    $$;
+    """)
+    session.execute('''
+    GRANT EXECUTE ON FUNCTION api.login(TEXT, TEXT) TO anon;
+    ''')
---
+++ scripts/setup_users_table.py
@@ -0,0 +1,67 @@
+def install_user_table_functions(session):
+    session.execute("GRANT SELECT ON TABLE auth.users TO anon;")
+
+    session.execute(f"""
+    CREATE OR REPLACE FUNCTION
+      auth.check_if_role_exists()
+      RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+      IF NOT exists(SELECT 1
+                    FROM pg_roles
+                    WHERE pg_roles.rolname = NEW.role)
+      THEN
+        RAISE foreign_key_violation
+        USING MESSAGE = 'Unknown database role: ' || NEW.role;
+        RETURN NULL;
+      END IF;
+      RETURN NEW;
+    END
+    $$;
+
+    DROP TRIGGER IF EXISTS ensure_user_role_exists
+    ON auth.users;
+    CREATE CONSTRAINT TRIGGER ensure_user_role_exists
+    AFTER INSERT OR UPDATE ON auth.users
+    FOR EACH ROW
+    EXECUTE PROCEDURE auth.check_if_role_exists();
+    """)
+
+    session.execute(f"""
+    CREATE OR REPLACE FUNCTION
+      auth.encrypt_password()
+      RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+      IF tg_op = 'INSERT' OR new.password <> old.password
+      THEN
+        new.password = crypt(new.password, gen_salt('bf', 8));
+      END IF;
+      RETURN new;
+    END
+    $$;
+
+    DROP TRIGGER IF EXISTS encrypt_password
+    ON auth.users;
+    CREATE TRIGGER encrypt_password
+    BEFORE INSERT OR UPDATE ON auth.users
+    FOR EACH ROW
+    EXECUTE PROCEDURE auth.encrypt_password();
+
+    CREATE OR REPLACE FUNCTION
+      auth.user_role(_email TEXT, _password TEXT)
+      RETURNS NAME
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+      RETURN (
+        SELECT role
+        FROM auth.users
+        WHERE users.email = _email
+              AND users.password = crypt(_password, users.password)
+      );
+    END;
+    $$;
+    """)
