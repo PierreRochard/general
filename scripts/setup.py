8c0b624f990439efc3fbf7e3a68fff0ffa9eccc3
Pierre Rochard <pierre@rochard.org>
2017-04-30 12:55:16 -0400

    debugging imports

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,18 +1,29 @@
+import sys
+sys.path.insert(0, '../')
+
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 from models import Base
 from scripts import pg_url
+from .setup_login import install_login_function
+from .setup_users_table import install_user_table_functions
+from .setup_notifications import setup_table_notifications
 
 
 def setup_database():
     engine = create_engine(pg_url, echo=True)
+    session = scoped_session(sessionmaker(bind=engine, autocommit=True))()
+    session.connection().connection.set_isolation_level(0)
+
     Base.metadata.create_all(bind=engine)
 
-    # install_user_table_functions(session)
-    #
-    # install_login_function(session)
-    # for schema, table in [('api', 'messages')]:
-    #     setup_table_notifications(session, schema, table)
+    install_user_table_functions(session)
+
+    install_login_function(session)
+
+    for schema, table in [('api', 'messages')]:
+        setup_table_notifications(session, schema, table)
 
 
 if __name__ == '__main__':
