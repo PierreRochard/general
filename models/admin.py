60078da70ccf30fe0c6beca71ff6dc738200da81
Pierre Rochard <pierre.rochard@axial.net>
2017-05-03 09:12:35 -0400

    adding column settings table

--- models/admin.py
+++ models/admin.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, text, UniqueConstraint, Integer
+from sqlalchemy import Column, String, text, UniqueConstraint, Integer, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 
 from .util import Base
@@ -38,7 +38,29 @@ class TableSettings(Base):
                 server_default=text('gen_random_uuid()'),
                 primary_key=True)
     user = Column(String)
+    table = Column(String)
     table_name = Column(String)
+    category = Column(String)
+    visible = Column(Boolean)
+
+
+class ColumnSettings(Base):
+    __tablename__ = 'column_settings'
+    __table_args__ = (UniqueConstraint('user',
+                                       'path',
+                                       'property',
+                                       'key',
+                                       name='setting_unique_constraint'),
+                      {'schema': 'admin'},
+                      )
+
+    id = Column(UUID(as_uuid=True),
+                server_default=text('gen_random_uuid()'),
+                primary_key=True)
+    user = Column(String)
+    table = Column(String)
+    column = Column(String)
     column_name = Column(String)
     index = Column(Integer)
     format = Column(String)
+    visible = Column(Boolean)
