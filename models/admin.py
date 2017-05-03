765559403e9d64ee65a5346913b6a1b80480112e
Pierre Rochard <pierre@rochard.org>
2017-05-02 21:49:18 -0400

    adding table settings table

--- models/admin.py
+++ models/admin.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, text, UniqueConstraint
+from sqlalchemy import Column, String, text, UniqueConstraint, Integer
 from sqlalchemy.dialects.postgresql import UUID
 
 from .util import Base
@@ -22,3 +22,23 @@ class Settings(Base):
     property = Column(String)
     key = Column(String)
     value = Column(String)
+
+
+class TableSettings(Base):
+    __tablename__ = 'table_settings'
+    __table_args__ = (UniqueConstraint('user',
+                                       'path',
+                                       'property',
+                                       'key',
+                                       name='setting_unique_constraint'),
+                      {'schema': 'admin'},
+                      )
+
+    id = Column(UUID(as_uuid=True),
+                server_default=text('gen_random_uuid()'),
+                primary_key=True)
+    user = Column(String)
+    table_name = Column(String)
+    column_name = Column(String)
+    index = Column(Integer)
+    format = Column(String)
