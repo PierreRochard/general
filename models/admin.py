19cfc58af96e7cbb98cba1d81bae014bce671981
Pierre Rochard <pierre.rochard@axial.net>
2017-05-04 09:08:56 -0400

    adding column settings table

--- models/admin.py
+++ models/admin.py
@@ -4,26 +4,6 @@ from sqlalchemy.dialects.postgresql import UUID
 from .util import Base
 
 
-class Settings(Base):
-    __tablename__ = 'settings'
-    __table_args__ = (UniqueConstraint('user',
-                                       'path',
-                                       'property',
-                                       'key',
-                                       name='setting_unique_constraint'),
-                      {'schema': 'admin'},
-                      )
-
-    id = Column(UUID(as_uuid=True),
-                server_default=text('gen_random_uuid()'),
-                primary_key=True)
-    user = Column(String)
-    path = Column(String)
-    property = Column(String)
-    key = Column(String)
-    value = Column(String)
-
-
 class TableSettings(Base):
     __tablename__ = 'table_settings'
     __table_args__ = (UniqueConstraint('user',
@@ -38,19 +18,29 @@ class TableSettings(Base):
                 server_default=text('gen_random_uuid()'),
                 primary_key=True)
     user = Column(String)
-    table = Column(String)
     table_name = Column(String)
+    custom_name = Column(String)
     category = Column(String)
     visible = Column(Boolean)
+
+
+def setup_table_settings_views(session):
+    session.execute("""
+        CREATE MATERIALIZED VIEW IF NOT EXISTS tables AS
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = 'api';
+        
+        REFRESH MATERIALIZED VIEW tables;
+    """)
 
 
 class ColumnSettings(Base):
     __tablename__ = 'column_settings'
     __table_args__ = (UniqueConstraint('user',
-                                       'path',
-                                       'property',
-                                       'key',
-                                       name='setting_unique_constraint'),
+                                       'table_name',
+                                       'column_name',
+                                       name='column_settings_unique_constraint'),
                       {'schema': 'admin'},
                       )
 
@@ -58,9 +48,20 @@ class ColumnSettings(Base):
                 server_default=text('gen_random_uuid()'),
                 primary_key=True)
     user = Column(String)
-    table = Column(String)
-    column = Column(String)
+    table_name = Column(String)
     column_name = Column(String)
+    custom_name = Column(String)
     index = Column(Integer)
     format = Column(String)
     visible = Column(Boolean)
+
+
+def setup_column_settings_views(session):
+    session.execute("""
+        CREATE MATERIALIZED VIEW IF NOT EXISTS columns AS
+        SELECT table_name, column_name, is_nullable, data_type
+        FROM information_schema.columns
+        WHERE table_schema = 'api'
+
+        REFRESH MATERIALIZED VIEW columns;
+    """)
