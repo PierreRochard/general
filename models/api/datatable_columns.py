691c0e0f6fe0cc4899973871ff9e9814c9135f30
Pierre Rochard <pierre.rochard@axial.net>
2017-06-26 08:47:56 -0400

    fixing table

--- models/api/datatable_columns.py
+++ models/api/datatable_columns.py
@@ -6,15 +6,15 @@ def create_api_datatable_columns_view():
         session.execute("""
           CREATE OR REPLACE VIEW api.datatable_columns AS
             SELECT c.table_name, 
-                   c.column_name as field, 
-                   coalesce(cs.custom_name, c.column_name) as header,
+                   c.column_name as value, 
+                   coalesce(cs.custom_name, c.column_name) as label,
                    cs.order_index
             FROM admin.columns c
               LEFT OUTER JOIN admin.table_column_settings cs
                 ON c.table_name = cs.table_name
                    AND c.column_name = cs.column_name
                    AND cs.user = current_user
-          ORDER BY order_index ASC NULLS LAST, field ASC NULLS LAST;
+          ORDER BY order_index ASC NULLS LAST;
         """)
 
         session.execute("""
