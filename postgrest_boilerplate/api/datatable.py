3dbd72aa79716b205c93f26b07eee293d2b38567
Pierre Rochard <pierre.rochard@axial.net>
2017-07-03 14:48:53 -0400

    refactoring views

--- postgrest_boilerplate/api/datatable.py
+++ postgrest_boilerplate/api/datatable.py
@@ -5,16 +5,13 @@ def create_api_datatable_view():
     with session_scope() as session:
         session.execute("""
           CREATE OR REPLACE VIEW api.datatable AS
-            SELECT t.table_name as name, 
-                   coalesce(ts.custom_name, t.table_name) as header,
-                   coalesce(ts.row_limit, 10) as "limit",
-                   coalesce(ts.row_offset, 0) as "offset",
+            SELECT ts.table_name AS name, 
+                   ts.custom_name AS header,
+                   ts.row_limit AS "limit",
+                   ts.row_offset AS "offset",
                    ts.sort_column,
-                   coalesce(ts.sort_order, 0) as sort_order
-            FROM admin.tables t
-              LEFT OUTER JOIN admin.table_settings ts
-                ON t.table_name = ts.table_name
-                   AND ts.user = current_user;
+                   ts.sort_order
+            FROM api.table_settings ts;
         """)
 
         session.execute("""
--- postgrest_boilerplate/api/table_settings.py
+++ postgrest_boilerplate/api/table_settings.py
@@ -5,24 +5,27 @@ def create_api_table_settings():
     with session_scope() as session:
         session.execute("""
         CREATE OR REPLACE VIEW api.table_settings AS 
-          SELECT t.table_name, 
-                 coalesce(ts.id, auth.gen_random_uuid()) as id,
+          SELECT coalesce(ts.id, auth.gen_random_uuid()) as id,
                  coalesce(ts."user", current_user) as "user",
+                 t.table_name,
+                 
+                 coalesce(ts.can_delete, TRUE) AS can_delete,
+                 coalesce(ts.can_insert, TRUE) AS can_insert,
+                 coalesce(ts.can_update, TRUE) AS can_update,
                  coalesce(ts.custom_name, initcap(replace(t.table_name, '_', ' '))) as custom_name,
                  ts.submenu_id,
                  coalesce(ts.icon, 'fa-table') AS icon,
                  coalesce(ts.is_visible, TRUE) AS is_visible,
-                 coalesce(ts.can_insert, TRUE) AS can_insert,
-                 coalesce(ts.can_update, TRUE) AS can_update,
-                 coalesce(ts.can_delete, TRUE) AS can_delete,
                  coalesce(ts.order_index, 0) AS order_index,
                  coalesce(ts.row_limit, 10) AS row_limit,
-                 coalesce(ts.row_offset, 0) as row_offset
+                 coalesce(ts.row_offset, 0) as row_offset,
+                 coalesce(ts.sort_column, 'id') as sort_column,
+                 coalesce(ts.sort_order, 1) as sort_order
           FROM admin.tables t
           LEFT OUTER JOIN admin.table_settings ts
               ON t.table_name = ts.table_name
               AND ts.user = current_user
-          ORDER BY ts.order_index ASC NULLS LAST, t.table_name ASC NULLS LAST;
+          ORDER BY ts.order_index ASC, t.table_name ASC;
         """)
 
         session.execute("""
