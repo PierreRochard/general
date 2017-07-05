773b4cb6a57a749a24c8079abe774e34db7357a9
Pierre Rochard <pierre.rochard@axial.net>
2017-07-05 10:00:06 -0400

    datatable updates

--- postgrest_boilerplate/api/datatable.py
+++ postgrest_boilerplate/api/datatable.py
@@ -27,27 +27,27 @@ def create_api_datatable_view():
         """)
 
         session.execute("""
-                    CREATE OR REPLACE FUNCTION admin.datatable_function()
-                      RETURNS TRIGGER AS
-                            $BODY$
-                               BEGIN
-                                  IF TG_OP = 'UPDATE' THEN
-                                  INSERT INTO admin.table_settings (table_name, row_offset, sort_column, sort_order)
-                                  VALUES (NEW.name, NEW."offset", NEW.sort_column, NEW.sort_order)
-                                  ON CONFLICT ("user", table_name)
-                                    DO UPDATE SET 
-                                           row_offset=NEW."offset",
-                                           sort_column=NEW.sort_column,
-                                           sort_order=NEW.sort_order
-                                    WHERE admin.table_settings.user = current_user
-                                      AND admin.table_settings.table_name = NEW.name;
-                                   RETURN NEW;
-                                END IF;
-                                RETURN NEW;
-                              END;
-                            $BODY$
-                      LANGUAGE plpgsql VOLATILE
-                      COST 100;
+            CREATE OR REPLACE FUNCTION admin.datatable_function()
+              RETURNS TRIGGER AS
+                    $BODY$
+                       BEGIN
+                          IF TG_OP = 'UPDATE' THEN
+                          INSERT INTO admin.table_settings (table_name, row_offset, sort_column, sort_order)
+                          VALUES (NEW.name, NEW."offset", NEW.sort_column, NEW.sort_order)
+                          ON CONFLICT ("user", table_name)
+                            DO UPDATE SET 
+                                   row_offset=NEW."offset",
+                                   sort_column=NEW.sort_column,
+                                   sort_order=NEW.sort_order
+                            WHERE admin.table_settings.user = current_user
+                              AND admin.table_settings.table_name = NEW.name;
+                           RETURN NEW;
+                        END IF;
+                        RETURN NEW;
+                      END;
+                    $BODY$
+              LANGUAGE plpgsql VOLATILE
+              COST 100;
                     """)
 
         session.execute("""
