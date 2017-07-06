c862e577eb6ac40a45172e7c017ed2dd30f45f90
Pierre Rochard <pierre.rochard@axial.net>
2017-07-06 08:28:47 -0400

    tightening up permissions

--- postgrest_boilerplate/api/menubar.py
+++ postgrest_boilerplate/api/menubar.py
@@ -8,18 +8,24 @@ from postgrest_boilerplate.models.util import session_scope
 
 
 def create_api_items():
+    """
+    NB: we can't use the api.table_settings view in the items view as
+    this will break our ability to join with submenu
+    """
     with session_scope() as session:
         session.execute("""
         CREATE OR REPLACE VIEW api.items AS 
-          SELECT ts.custom_name AS label,
-                 ts.icon,
-                 ts.id,
+          SELECT coalesce(ts.custom_name, initcap(replace(t.table_name, '_', ' '))) as label,
+                 coalesce(ts.icon, 'fa-table') AS icon,
+                 coalesce(ts.id, auth.gen_random_uuid()) as id,
                  ts.submenu_id,
-                 string_to_array('/' || ts.table_name, ' ') AS "routerLink",
-                 ts.is_visible,
-                 ts.order_index
-          FROM api.table_settings ts
-          WHERE current_user != 'anon'
+                 string_to_array('/' || t.table_name, ' ') AS "routerLink",
+                 coalesce(ts.order_index, 0) AS order_index
+          FROM admin.tables t
+          LEFT OUTER JOIN admin.table_settings ts
+              ON t.table_name = ts.table_name
+              AND ts.user = current_user
+          WHERE current_user != 'anon' AND ts.is_visible
           UNION
           SELECT coalesce(fs.custom_name, f.form_name) as label,
                           fs.icon,
@@ -27,7 +33,6 @@ def create_api_items():
                           fs.submenu_id AS submenu_id,
                                  string_to_array('/rpc/' || fs.form_name, ' ') 
                                     AS "routerLink",
-                          fs.is_visible,
                           fs.order_index
           FROM admin.forms f
           LEFT OUTER JOIN admin.form_settings fs
@@ -35,6 +40,7 @@ def create_api_items():
             AND fs.user = current_user
           WHERE (current_user != 'anon' AND f.form_name != 'login')
              OR (current_user  = 'anon' AND f.form_name  = 'login')
+             AND fs.is_visible
           ORDER BY order_index ASC NULLS LAST, label ASC NULLS LAST;
           """)
 
@@ -51,17 +57,16 @@ def create_api_submenus():
                 s.submenu_name AS label,
                 s.icon,
                 string_to_array('', '') as "routerLink",
-                s.is_visible,
                 s.order_index
          FROM admin.submenus s
          WHERE s.user = current_user
+           AND s.is_visible
            AND current_user != 'anon'
          UNION
          SELECT i.id,
                 i.label,
                 i.icon, 
                 i."routerLink",
-                i.is_visible,
                 i.order_index
          FROM api.items i
          WHERE i.submenu_id IS NULL
@@ -69,5 +74,5 @@ def create_api_submenus():
         """)
 
         session.execute("""
-        GRANT SELECT, UPDATE, INSERT ON api.menubar TO anon;
+        GRANT SELECT ON api.menubar TO anon;
         """)
