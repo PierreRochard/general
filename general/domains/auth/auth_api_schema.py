f1a294174d0fce92352af241a3ea0d7f502af784
Pierre Rochard <pierre@rochard.org>
2017-08-26 12:04:55 -0400

    setting up auth_api schema

--- general/domains/auth/auth_api_schema.py
+++ general/domains/auth/auth_api_schema.py
@@ -2,7 +2,10 @@ from general.database.schema import Schema
 from general.database.util import session_scope
 from general.domains.auth.models import Users
 
-from .api_functions import create_login_api_trigger, create_logout_api_trigger
+from general.domains.auth.api_functions import (
+    create_login_api_trigger,
+    create_logout_api_trigger
+)
 
 
 class AuthApiSchema(Schema):
@@ -10,9 +13,10 @@ class AuthApiSchema(Schema):
     def __init__(self):
         super(AuthApiSchema, self).__init__(name='auth_api')
 
-    def create_functions(self):
-        create_login_api_trigger(self.name)
-        create_logout_api_trigger(self.name)
+    @staticmethod
+    def create_functions():
+        create_login_api_trigger()
+        create_logout_api_trigger()
 
     def grant_auth_privileges(self):
         with session_scope() as session:
@@ -20,13 +24,21 @@ class AuthApiSchema(Schema):
                 'SCHEMA': {
                     'auth_api': {
                         'USAGE': [u.role for u in session.query(Users).all()]
-                                     .append('anon')
                     }
                 },
                 'FUNCTION': {
-                    'login(TEXT, TEXT)': {
+                    'auth_api.login(TEXT, TEXT)': {
                         'EXECUTE': ['anon']
-                    }
-                }
+                    },
+                    'auth_api.logout()': {
+                        'EXECUTE': [u.role for u in session.query(Users).all()]
+                    },
+                },
             }
         self.grant_privileges(self.name, privileges)
+
+if __name__ == '__main__':
+    auth_api_schema = AuthApiSchema()
+    auth_api_schema.create_schema()
+    auth_api_schema.create_functions()
+    auth_api_schema.grant_auth_privileges()
