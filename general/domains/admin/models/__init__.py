2e82d3a6cce67c16e6af882e31889cb16256a511
Pierre Rochard <pierre.rochard@axial.net>
2018-01-07 12:47:19 -0500

    add notification message settings

--- general/domains/admin/models/__init__.py
+++ general/domains/admin/models/__init__.py
@@ -3,6 +3,7 @@ from .feature_sets_users import FeatureSetsUsers
 from .form_field_settings import FormFieldSettings
 from .form_settings import FormSettings
 from .mapper_settings import MapperSettings
+from .notification_message_settings import NotificationMessageSettings
 from .submenus import Submenus
 from .table_column_settings import TableColumnSettings
 from .table_settings import TableSettings
---
+++ general/domains/admin/models/notification_message_settings.py
@@ -0,0 +1,32 @@
+from sqlalchemy import (Boolean, Column, ForeignKey, String, text,
+                        UniqueConstraint)
+from sqlalchemy.dialects.postgresql import UUID
+
+from general.database.base import Base
+
+
+class NotificationMessageSettings(Base):
+    __tablename__ = 'notification_message_settings'
+    __table_args__ = (UniqueConstraint('user_id',
+                                       'namespace',
+                                       'message_type',
+                                       name='notification_message_settings_unique_constraint'),
+                      {'schema': 'admin'},
+                      )
+
+    id = Column(UUID,
+                server_default=text('auth.gen_random_uuid()'),
+                primary_key=True)
+    namespace = Column(String, nullable=False)
+    message_type = Column(String, nullable=False)
+
+    is_visible = Column(Boolean, nullable=False, default=False)
+    severity = Column(String, nullable=False, default='info')
+    summary = Column(String)
+    detail = Column(String)
+
+    user_id = Column(UUID,
+                     ForeignKey('auth.users.id',
+                                onupdate='CASCADE',
+                                ondelete='CASCADE'),
+                     nullable=False)
