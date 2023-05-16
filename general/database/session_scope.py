4c64ea3a273a611f3dce1b1773fa5c976b279deb
Pierre Rochard <prochard@riot.inc>
2023-05-16 09:06:38 -0500

    Upload to mssql

--- general/database/session_scope.py
+++ general/database/session_scope.py
@@ -38,3 +38,38 @@ def session_scope(echo=False,
         raise
     finally:
         session.close()
+
+
+@contextmanager
+def mssql_session_scope(echo=False,
+                        raise_integrity_error=True,
+                        raise_programming_error=True):
+    """Provide a transactional scope around a series of operations."""
+    ms_url = URL(drivername='mssql+pyodbc',
+                 username=os.environ['MSSQL_USER'],
+                 password=os.environ['PGPASSWORD'],
+                 host=os.environ['MSSQL_HOST'],
+                 port=os.environ['MSSQL_PORT'],
+                 database=os.environ['MSSQL_DB'],
+                 query={'driver': 'ODBC Driver 18 for SQL Server',
+                        'TrustServerCertificate': 'yes'})
+    engine = create_engine(ms_url, echo=echo)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    try:
+        yield session
+        session.commit()
+    except IntegrityError:
+        session.rollback()
+        if raise_integrity_error:
+            raise
+    except ProgrammingError:
+        session.rollback()
+        if raise_programming_error:
+            raise
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
