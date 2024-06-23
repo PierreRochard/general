1a5df137350ca948c112efbec5a7afead54e3ad0
Pierre Rochard <pierre@rochard.org>
2024-06-23 12:30:35 -0400

    Add wall clock

--- general/database/session_scope.py
+++ general/database/session_scope.py
@@ -12,12 +12,15 @@ def session_scope(echo=False,
                   raise_integrity_error=True,
                   raise_programming_error=True):
     """Provide a transactional scope around a series of operations."""
-    pg_url = URL(drivername='postgresql+psycopg2',
-                 username=os.environ['PGUSER'],
-                 password=os.environ['PGPASSWORD'],
-                 host=os.environ['PGHOST'],
-                 port=os.environ['PGPORT'],
-                 database=os.environ['PGDB'])
+    pg_url = URL.create(
+        drivername='postgresql+psycopg2',
+        username=os.environ['PGUSER'],
+        password=os.environ['PGPASSWORD'],
+        host=os.environ['PGHOST'],
+        port=int(os.environ['PGPORT']),
+        query={},
+        database=os.environ['PGDB']
+    )
     engine = create_engine(pg_url, echo=echo)
     session_maker = sessionmaker(bind=engine)
     session = session_maker()
